b7e55fdcff4b1de04d050029f4aa8bbaa0624938
GoodGuyHeffy <95945343+GoodGuyHeffy@users.noreply.github.com>
2023-08-13 15:27:36 -0500

    in progress

---
+++ src/pages/Booking.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Booking() {
+  return (
+    <div>
+      <form>
+        <div>
+            <label>Destinations</label>
+            <select name="" id=""></select>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Booking;
